981190d0545d92bb7ce8614789ccaa63216a75fe
Cristian Sáez Mardones <cristiansaez97@gmail.com>
2023-01-29 18:35:44 -0300

    Se extrae la información de pruebas a un archivo externo#

--- app-colecciones/src/components/index/Index.jsx
+++ app-colecciones/src/components/index/Index.jsx
@@ -2,44 +2,16 @@
 import React, { Fragment } from 'react';
 // Mis importaciones
 import { Collection } from '../coleccion/Collection';
+import { elementosPrueba } from '../../data/elements';
 
 export const Index = () => {
-    const elementosPrueba = [
-        {
-            title: 'Elemento 1',
-            text: 'Elemento de pruebas número 1',
-            involucrados: ['yo', 'pepe'],
-            autor: 'juanito',
-            category: 'cat1',
-            fecha: new Date(2023, 1, 2),
-            cover: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b7/2a/fe/volcan-osorno.jpg?w=1200&h=1200&s=1'
-        },
-        {
-            title: 'Elemento 2',
-            text: 'Elemento de pruebas número 2',
-            involucrados: ['yo', 'francica'],
-            autor: 'juanito',
-            category: 'cat1',
-            fecha: new Date(2023, 1, 4),
-            cover: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b7/2a/fe/volcan-osorno.jpg?w=1200&h=1200&s=1'
-        },
-        {
-            title: 'Elemento 3',
-            text: 'Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3Elemento de pruebas número 3',
-            involucrados: ['yo', 'francica'],
-            autor: 'juanito',
-            category: 'cat2',
-            fecha: new Date(2023, 1, 4),
-            cover: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b7/2a/fe/volcan-osorno.jpg?w=1200&h=1200&s=1'
-        }
-    ];
     return (
         <Fragment>
             <h2 style={{textAlign: 'center', marginTop: '10px'}}>Últimos registros</h2>
             <hr />
             <div className="collections">
                 {
-                    elementosPrueba.map((element, index) => (
+                    elementosPrueba.slice(0, 3).map((element, index) => (
                         <Collection key={index} collection={element} />
                     ))
                 }
